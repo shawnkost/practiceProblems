506261cd44bbb3b8f28e92ba03cac12b401899c9
Shawn Kost <shawnmkost@gmail.com>
2021-03-17 15:26:56 -0700

    Completed containsDuplicates on leetcode

---
+++ leetCode/containsDuplicates.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  return new Set(nums).size !== nums.length;
+};
+
+//check if Set is the same size as the nums arr length
