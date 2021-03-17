6175c1771691051b284e05be2b38cb06f2893a64
Shawn Kost <shawnmkost@gmail.com>
2021-03-17 14:49:54 -0700

    Completed removeDuplicates on leetcode

---
+++ leetCode/removeDuplicates.js
@@ -0,0 +1,18 @@
+/* Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns
+the new length.Do not allocate extra space for another array, you must do this by modifying the input array
+in-place with O(1) extra memory.
+
+ @param {number[]} nums
+ @return {number}
+
+ */
+const removeDuplicates = function(nums) {
+    let i = 0
+    for (j = 1; j < nums.length; j++) {
+        if (nums[i] != nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+};
