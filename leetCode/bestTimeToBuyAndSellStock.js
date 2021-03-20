3f3157adc96635c8ce2f3fccac37140d19978206
Shawn Kost <shawnmkost@gmail.com>
2021-03-20 15:00:14 -0700

    Completed bestTimeToBuyAndSellStock on leetcode

---
+++ leetCode/bestTimeToBuyAndSellStock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let profit = 0;
+  let min = prices[0];
+
+  for (let i = 1; i < prices.length; i++) {
+    if (min > prices[i]) {
+      min = prices[i];
+    } else if (prices[i] - min > profit) {
+      profit = prices[i] - min;
+    }
+  }
+
+  return profit;
+};
